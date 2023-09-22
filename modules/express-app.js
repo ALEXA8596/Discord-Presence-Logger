06c56ba51a580f876954d8fa855fa1aea9dd0564
ALEXA8596 <120238193+ALEXA8596@users.noreply.github.com>
2023-09-21 21:23:52 -0700

    Added Image Slider

--- modules/express-app.js
+++ modules/express-app.js
@@ -3,6 +3,8 @@ const app = express();
 const { fetch } = require('undici');
 
 module.exports = (port) => {
+    app.set("view engine", "ejs");	
+
     app.get('/timezone/:timezone', async (req, res) => {
         // console.log(req.params);
         fetch('https://worldtimeapi.org/api/timezone/' + req.params.timezone)
@@ -11,6 +13,12 @@ module.exports = (port) => {
                 res.json(json);
             });
     });
+
+    // return an image slider of before and after images
+    app.get('/slider', async (req, res) => {
+        res.render("slider", { before: req.query.before, after: req.query.after });
+    });
+
     app.listen(port, () => {
         console.log(`Express server listening on port ${port}`);
     });
